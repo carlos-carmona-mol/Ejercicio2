626bbd6e44d2ba783ee1edbcb2486407030f460f
Carlos <ccarmonm@emeal.nttdata.com>
2026-04-22 14:38:46 +0200

    Creado viewmodel

---
+++ app/src/main/java/com/example/ejercicio2/FormViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.ejercicio2
+
+import android.util.Patterns
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FormViewModel : ViewModel() {
+    val titulo = MutableLiveData("")
+    val descripcion = MutableLiveData("")
+    val categoria = MutableLiveData("")
+    val prioridad = MutableLiveData("")
+    val email = MutableLiveData("")
+    val formularioValido = MutableLiveData(false)
+
+    fun validar() {
+        val ti = titulo.value!!.length in 5..60
+        val de = descripcion.value!!.length in 20..500
+        val pr = prioridad.value!!.toIntOrNull() in 1..5
+        val em = Patterns.EMAIL_ADDRESS.matcher(email.value!!).matches()
+
+        formularioValido.value = ti && de && pr && em
+    }
+}
