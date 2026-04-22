4e1290f71a81d6acffccc6b2c09b47852ef1d749
Carlos <ccarmonm@emeal.nttdata.com>
2026-04-22 15:17:35 +0200

    Prueba de formulario enviado

--- app/src/main/java/com/example/ejercicio2/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio2/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.ejercicio2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+
+    private val vm : FormViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val titulo = findViewById<EditText>(R.id.inputTitulo)
+        val descripcion = findViewById<EditText>(R.id.inputDescripcion)
+        val categoria = findViewById<EditText>(R.id.inputCategoria)
+        val prioridad = findViewById<EditText>(R.id.inputPrioridad)
+        val email = findViewById<EditText>(R.id.inputEmail)
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+
+        vm.formularioValido.observe(this) {valido ->
+            btnEnviar.isEnabled = valido
         }
+
+        titulo.addTextChangedListener{
+            vm.titulo.value = it.toString()
+            vm.validar()
+        }
+
+        descripcion.addTextChangedListener{
+            vm.descripcion.value = it.toString()
+            vm.validar()
+        }
+
+        categoria.addTextChangedListener{
+            vm.categoria.value = it.toString()
+            vm.validar()
+        }
+
+        prioridad.addTextChangedListener{
+            vm.prioridad.value = it.toString()
+            vm.validar()
+        }
+
+        email.addTextChangedListener{
+            vm.email.value = it.toString()
+            vm.validar()
+        }
+
+        btnEnviar.setOnClickListener {
+            enviarDatos()
+        }
+    }
+
+    fun enviarDatos() {
+        Toast.makeText(this, "Enviando Formulario.....", Toast.LENGTH_SHORT).show()
     }
 }
